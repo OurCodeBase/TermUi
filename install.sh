98e76010cb9902191914ba5b30d99964293dceac
Strange Code <100824266+strangecode4u@users.noreply.github.com>
2023-03-13 17:49:37 +0530

    update install.sh

--- install.sh
+++ install.sh
@@ -106,8 +106,7 @@ phase1(){
 
 starts(){
   clear;echo;
-  if [[ ! -d "${HOME}/storage/shared" ]]; then bl -s "Storage Permission is already Allowed...";
-  else bl -a "Please Allow Storage Permission...";eval "termux-setup-storage";fi
+  if [[ -d "${HOME}/storage/shared" ]]; then bl -s "Please Allow Storage Permission...";eval "termux-setup-storage";fi
   mkdir -p "${HOME}/.ui";pkg_build git;pkg_build zsh;phase1;return 0;
 }
 
