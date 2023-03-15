1b447b8bdde70f731c7975faf66dd9393e5cd39b
Harsh B <harshb7839@gmail.com>
2023-03-15 15:29:47 +0530

    Update 1.1.0

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#/bin/bash
+
+# Variables
+red="\e[0;31m";green="\e[0;32m";blue="\e[0;34m";TermDir="${HOME}/.termux";
+alert="\e[0;31m\e[1m[∆]";success="\e[0;32m\e[1m[√]";
+pearly="\e[0;2m";enc="\e[0;m";lisence="${TermDir}/lisence.txt";
 
 bl(){
   while getopts 'rgbpfias:h' opt; do
@@ -18,33 +23,15 @@ bl(){
   else echo -e "${var3}${2}\e[0;m";return 0;fi
 }
 
-Spin(){
-  echo;_PID=${!};i=1;_spins=('█■■■■' '■█■■■' '■■█■■' '■■■█■' '■■■■█');echo -n ' ';
-  while [ -d /proc/${_PID} ];do
-  for _snip in ${_spins[@]} ; do echo -ne "\r\e[0;2mLoading...[${_snip}]\e[0;m ";sleep 0.2;done;done;echo;echo;return;
-}
-
 pkg_build(){
   pkg_info=${1};
   if [[ -z "${pkg_info}" ]]; then echo;bl -a "Package parameter is Empty...";echo;return 1;fi
   (ping -c 3 google.com) &> /dev/null 2>&1;
-  if [[ "${?}" != 0 ]]; then echo;bl -a "Internet Connection Error...";echo;return 1;fi
+  if [[ "${?}" != 0 ]]; then echo;bl -a "Internet Connection Error...";echo;exit;fi
   _pkg_var2=$(eval "apt search ${pkg_info} 2> /dev/null");
   if [[ "${_pkg_var2}" != *"stable"* ]]; then echo;bl -a "Package does not Exist...";echo;return 1;fi
-  count=0;total=34;pstr="[======================================]";echo;bl -s "Installing ${pkg_info}...";echo -e "\e[0;2m";
-  while [ $count -lt $total ]; do
-    eval "apt-get install ${pkg_info} -y &> /dev/null || sudo apt-get install ${pkg_info} -y &> /dev/null";
-    count=$(( $count + 1 ));pd=$(( $count * 73 / $total ));
-    printf "\r%3d.%1d%% %.${pd}s" $(( $count * 100 / $total )) $(( ($count * 1000 / $total) % 10 )) $pstr;done;echo -e "\e[0;m";echo;return 0;
-}
-
-dnload(){
-  _file_link=${1};prova="curl -OL "; # prova = process variable
-  if [[ -z "${_file_link}" ]]; then echo;bl -a "Link parameter is Empty...";echo;return 1;fi
-  (ping -c 3 google.com) &> /dev/null 2>&1;
-  if [[ "${?}" != 0 ]]; then echo;bl -a "Internet Connection Error...";echo;return 1;fi
-  prova+="${_file_link} &> /dev/null";echo;bl -s "Downloading File...";
-  (eval "${prova}" & Spin) || (echo;bl -a "Unknown Error..."echo;return 1;)
+  echo;bl -s "Installing ${pkg_info}...";echo -e "${pearly}${pearly}";
+  (eval "apt-get install ${pkg_info} -y" || eval "sudo apt-get install ${pkg_info} -y");echo -e "${enc}";return 0;
 }
 
 dnrepo(){
@@ -53,61 +40,110 @@ dnrepo(){
   if [[ -z "${_file_link}" ]]; then echo;bl -a "File parameter is Empty...";echo;return 1;fi
   (ping -c 3 google.com) &> /dev/null 2>&1;
   if [[ "${?}" != 0 ]]; then echo;bl -a "Internet Connection Error...";echo;return 1;fi
-  prova+="${_repo_link}.git ${_file_link} --depth 1 &> /dev/null";
-  count=0;total=34;pstr="[======================================]";echo;bl -s "Cloning ${_repo_link}...";echo -e "\e[0;2m";
-  while [ $count -lt $total ]; do
-    eval "${prova}";count=$(( $count + 1 ));pd=$(( $count * 73 / $total ));
-    printf "\r%3d.%1d%% %.${pd}s" $(( $count * 100 / $total )) $(( ($count * 1000 / $total) % 10 )) $pstr;done;echo -e "\e[0;m";echo;return 0;
+  prova+="${_repo_link}.git ${_file_link} --depth 1";
+  echo;bl -s "Cloning ${_repo_link}...";echo -e "${pearly}";
+  eval "${prova}";echo -e "${enc}";return 0;
 }
 
-phase2(){
-  if [[ -f "${HOME}/.ui/p2.dl" ]]; then return 0;
-  else
-    {
-      dnrepo "ohmyzsh/ohmyzsh" "${HOME}/.oh-my-zsh";
-      dnrepo "zsh-users/zsh-syntax-highlighting" "${HOME}/.zsh-syntax-highlighting";
-    } && {
-      if [[ -e "${HOME}/.zshrc" ]]; then
-        mv "${HOME}/.zshrc" "${HOME}/.zshrc.bak.$(date +%Y.%m.%d-%H:%M:%S)"
-      fi
-      cp "${HOME}/.oh-my-zsh/templates/zshrc.zsh-template" "${HOME}/.zshrc"
-      sed -i '/^ZSH_THEME/d' "${HOME}/.zshrc"
-      sed -i '1iZSH_THEME="agnoster"' "${HOME}/.zshrc"
-      echo "alias chcolor='${HOME}/.termux/colors.sh'" >> "${HOME}/.zshrc"
-      echo "alias chfont='${HOME}/.termux/fonts.sh'" >> "${HOME}/.zshrc"
-    } && {
-      echo "source ${HOME}/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "${HOME}/.zshrc"
-    } && {
-      chsh -s zsh
-    } && {
-      chmod +x ${HOME}/.termux/colors.sh
-      chmod +x ${HOME}/.termux/fonts.sh
-      echo -e "\e[0;m"
-      ${HOME}/.termux/colors.sh
-      ${HOME}/.termux/fonts.sh
-      echo
-      echo "1" >> "${HOME}/.ui/p2.dl"
-      bl -s "Please restart Termux app..."
-      rm -rf ${HOME}/../usr/etc/motd* &> /dev/null
-      echo
-      return 0
-    }
+banner(){
+  echo "
+╔═╗╔╦╗╦═╗╔═╗╔╗╔╔═╗╔═╗
+╚═╗ ║ ╠╦╝╠═╣║║║║ ╦║╣ 
+╚═╝ ╩ ╩╚═╩ ╩╝╚╝╚═╝╚═╝
+  ";
+  echo "Start coding with yourself.";
+  echo "___________________________";
+}
+
+is_userland(){
+  (cd /host-rootfs/data/data/tech.ula/files/home) &> /dev/null 2>&1;
+  if [[ "${?}" != 0 ]]; then return 1;else return 0;fi
+}
+
+lisence_exist(){
+  if [[ ! -f "${lisence}" ]]; then return 1;fi
+  if [[ $(cat ${lisence}) == *"Author:Harsh B"* ]]; then return 0;else return 1;fi
+}
+
+TermDir_Download(){
+  if [[ -d "${TermDir}" ]]; then mv "${TermDir}" "${TermDir}.bak.$(date +%Y.%m.%d-%H:%M:%S)";fi
+  (ping -c 3 google.com) &> /dev/null 2>&1;
+  if [[ "${?}" != 0 ]]; then echo;bl -a "Internet Connection Error...";echo;return 1;fi
+  prova="curl -OL https://github.com/strangecode4u/TermUi/raw/main/TermUi.zip";
+  echo;bl -s "Downloading File...";echo -e "${pearly}${pearly}";(eval "${prova}");echo -e "${enc}";
+  bl -s "Unpacking Files...";echo -e "${pearly}";(unzip -d ${HOME} TermUi.zip);
+  rm TermUi.zip;echo "TermDir:True" >> ${lisence};return 0;
+}
+
+install_ohmyzsh(){
+  pkg_build git;
+  dnrepo "ohmyzsh/ohmyzsh" "${HOME}/.oh-my-zsh";
+  if [[ -e "${HOME}/.zshrc" ]]; then
+  mv "${HOME}/.zshrc" "${HOME}/.zshrc.bak.$(date +%Y.%m.%d-%H:%M:%S)";fi
+  cp "${HOME}/.oh-my-zsh/templates/zshrc.zsh-template" "${HOME}/.zshrc";
+  sed -i '/^ZSH_THEME/d' "${HOME}/.zshrc";
+  sed -i '1iZSH_THEME="agnoster"' "${HOME}/.zshrc";
+}
+
+install_zsh(){
+  pkg_build zsh;
+  if is_userland; then
+    echo "su" >> ~/.bashrc;echo "zsh" >> /root/.bashrc;
+    bl -s "Please restart your Userland session...";echo;
+  else chsh -s zsh;
+    bl -s "Please restart your Termux session...";echo;
   fi
 }
 
-phase1(){
-  if [[ -f "${HOME}/.ui/p1.dl" ]]; then phase2;return 0;
+install_color(){
+  if ! lisence_exist; then TermDir_Download;starter;return 1;fi
+  echo;color_array=();cd "${TermDir}/colors";let i=0;
+  for file in *.properties ; do color_array=(${color_array[@]} "${file}");done;cd;
+  for obj in ${color_array[@]} ; do
+  obj=${obj/".properties"/};echo "[$((i++))] ${obj}";done;
+  echo;read -p ">> " choice;
+  if [[ "${choice}" -ge "${#color_array[@]}" ]]; then
+  echo;bl -a "Invalid Input...";echo;return 1;fi
+  if is_userland; then
+    hostdir="/host-rootfs/data/data/tech.ula/files/home";mkdir -p ${hostdir}/.termux;
+    yes | cp "${TermDir}/colors/${color_array[${choice}]}" "${hostdir}/.termux/colors.properties";echo;
+    bl -s "Please restart your Userland session..."echo;
   else
-    if [[ -d "${HOME}/.termux" ]]; then mv "${HOME}/.termux" "${HOME}/.termux.bak.$(date +%Y.%m.%d-%H:%M:%S)";fi
-    dnload "https://github.com/strangecode4u/TermUi/raw/main/TermUi.zip";
-    bl -s "Unpacking Files...";(unzip -d ${HOME} TermUi.zip &> /dev/null) & Spin
-    rm TermUi.zip;echo "1" > ${HOME}/.ui/p1.dl;phase2;return 0;fi
+    yes | cp "${TermDir}/colors/${color_array[${choice}]}" "${TermDir}/colors.properties";
+    eval "termux-reload-settings";
+    echo;bl -s "Please restart your Termux session...";echo;return 0;
+  fi
 }
 
-starts(){
-  clear;echo;
-  if [[ ! -d "${HOME}/storage/shared" ]]; then bl -s "Please Allow Storage Permission...";eval "termux-setup-storage";fi
-  mkdir -p "${HOME}/.ui";pkg_build git;pkg_build zsh;phase1;return 0;
+install_font(){
+  if ! lisence_exist; then TermDir_Download;starter;return 1;fi
+  echo;font_array=();cd "${TermDir}/fonts";let i=0;
+  for file in *.ttf ; do font_array=(${font_array[@]} "${file}");done;cd;
+  for obj in ${font_array[@]} ; do
+  obj=${obj/".ttf"/};echo "[$((i++))] ${obj}";done;
+  echo;read -p ">> " choice;
+  if [[ "${choice}" -ge "${#font_array[@]}" ]]; then
+  echo;bl -a "Invalid Input...";echo;return 1;fi
+  if is_userland; then
+    hostdir="/host-rootfs/data/data/tech.ula/files/home";mkdir -p ${hostdir}/.termux;
+    yes | cp "${font_array[${choice}]}" "${hostdir}/.termux/font.ttf";echo;
+    bl -s "Please restart your Userland session..."echo;
+  else
+    yes | cp "${TermDir}/fonts/${font_array[${choice}]}" "${TermDir}/font.ttf";
+    eval "termux-reload-settings";
+    echo;bl -s "Please restart your Termux session...";echo;return 0;
+  fi
 }
 
-starts
+starter(){
+  cd;banner;let i=0;echo;
+  funcs_array=("Font" "Colour" "Zsh" "Zsh_Syntax_Highlighting" "OhMyZsh");
+  for funcs in ${funcs_array[@]} ; do echo "[$((i++))] Install ${funcs}";done;
+  echo;read -p ">> " choice;
+  case ${choice} in
+    0)install_font;;1)install_color;;
+    2)install_zsh;;3)install_zsh_syntax;;4)install_ohmyzsh;;
+    *)echo;bl -a "Invalid Input...";echo;return 1;;esac
+}
+
+starter
