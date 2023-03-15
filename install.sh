e63b2dce2fc73e31002c150271c80b7a16880d53
Strange Code <100824266+strangecode4u@users.noreply.github.com>
2023-03-15 22:59:31 +0530

    update install.sh

--- install.sh
+++ install.sh
@@ -68,7 +68,7 @@ TermDir_Download(){
   if [[ -d "${TermDir}" ]]; then mv "${TermDir}" "${TermDir}.bak.$(date +%Y.%m.%d-%H:%M:%S)";fi
   prova="curl -OL https://github.com/strangecode4u/TermUi/raw/main/TermUi.zip";
   echo;bl -s "Downloading File...";echo -e "${pearly}${pearly}";(eval "${prova}");
-  if [[ "${?}" != 0 ]]; then echo;bl -a "Download Failed...";echo;return 1;fi
+  if [[ "${?}" != 0 ]]; then echo;bl -a "Download Failed...";echo;exit;fi
   echo -e "${enc}";
   bl -s "Unpacking Files...";echo -e "${pearly}";(unzip -d ${HOME} TermUi.zip);
   if [[ "${?}" != 0 ]]; then echo;bl -a "Unpacking Failed...";echo;return 1;fi
