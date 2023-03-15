7368c9af1c49afa6ca9fe7de5852f5c685b74cf1
Strange Code <100824266+strangecode4u@users.noreply.github.com>
2023-03-15 16:11:48 +0530

    update install.sh

--- install.sh
+++ install.sh
@@ -108,8 +108,8 @@ install_color(){
   echo;bl -a "Invalid Input...";echo;return 1;fi
   if is_userland; then
     hostdir="/host-rootfs/data/data/tech.ula/files/home";mkdir -p ${hostdir}/.termux;
-    (yes | cp -f "${TermDir}/colors/${color_array[${choice}]}" "${hostdir}/.termux/colors.properties") &> /dev/null;echo;
-    bl -s "Please restart your Userland session...";echo;
+    (yes | cp -f "${TermDir}/colors/${color_array[${choice}]}" "${hostdir}/.termux/colors.properties") &> /dev/null;
+    echo;bl -s "Please restart your Userland session...";echo;
   else
     yes | cp "${TermDir}/colors/${color_array[${choice}]}" "${TermDir}/colors.properties";
     eval "termux-reload-settings";
