3602603ba9af301270a3accb8cd7ccd554257a8e
Strange Code <100824266+strangecode4u@users.noreply.github.com>
2023-03-20 12:37:03 +0530

    update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,12 @@
-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
+#/bin/bash
+
+launcher(){
+  echo;wget "TermUi" "https://raw.githubusercontent.com/strangecode4u/TermUi/main/TermUi.sh";
+  if [[ "${?}" == 0 ]]; then
+    mv "TermUi" "${PREFIX}/bin/";
+    chmod +x "${PREFIX}/bin/TermUi";
+    echo;echo "TermUi";echo;exit;
+  else echo;exit;
+  fi
+}
+launcher
